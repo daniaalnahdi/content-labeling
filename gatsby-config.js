b0b4883b8a877967b1b0682d169219a0cda12bd7
daniaalnahdi <alnahdi.d@husky.neu.edu>
2020-11-11 22:43:49 +0300

    update gatsby configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,14 +10,14 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/images/`,
       },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `platforms`,
-        path: `${__dirname}/src/platforms/`,
+        path: `${__dirname}/src/pages/platforms/`,
       },
     },
     `gatsby-plugin-netlify-cms`,
