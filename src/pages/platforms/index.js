f456c9df7e77d73666cee181942d3704a0fec141
daniaalnahdi <alnahdi.d@husky.neu.edu>
2020-11-11 23:04:04 +0300

    add platforms page

---
+++ src/pages/platforms/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../../components/layout"
+import SEO from "../../components/seo"
+
+
+const PlatformsPage = () => (
+  <Layout>
+    <SEO title="Platforms" />
+    <h1>List of Platforms</h1>
+  </Layout>
+)
+
+export default PlatformsPage
